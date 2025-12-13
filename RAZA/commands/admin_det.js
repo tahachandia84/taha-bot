b9d5dbcd2dafaf5e17abd3417eac6e31be7efc85
tahachandia84 <tahachandia84@gmail.com>
2025-12-13 23:54:56 +0500

    Update admin_det.js

--- RAZA/commands/admin_det.js
+++ RAZA/commands/admin_det.js
@@ -18,7 +18,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
     var msg = {
         body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 🫅",
         attachment: 
-fs.createReadStream(__dirname + `/noprefix/IMG_20250608_082927.jpg`)
+fs.createReadStream(__dirname + `/no.`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🫅", event.messageID, (err) => {}, true)
